5d9761d3548cc23cf86ed5a22b8841271c38d466
Justin McKenzie <mckenjus@gmail.com>
2021-09-17 10:21:58 -0600

    added long word

--- runtime.js
+++ runtime.js
@@ -33,14 +33,33 @@ function getSizedArray(size){
     return array
 }
 
-
+console.time('test 10')
 const tinyArray = getSizedArray(10);
+console.timeEnd('test 10')
+console.time('test 100')
 const smallArray = getSizedArray(100);
+console.timeEnd('test 100')
+console.time('test 1000')
 const mediumArray = getSizedArray(1000);
+console.timeEnd('test 1000')
+console.time('test 10000')
 const largeArray = getSizedArray(10000);
+console.timeEnd('test 10000')
+console.time('test 100000')
 const extraLargeArray = getSizedArray(100000);
+console.timeEnd('test 100000')
+// console.time('test 6')
+// const extraextraLargeArray = getSizedArray(100000000);
+// console.timeEnd('test 6')
 
 
+
+
+// test 10: 0.148ms, 0.114ms
+// test 100: 0.023ms, .02ms
+// test 1000: 0.066ms, 0.058ms
+// test 10000: 1.455ms, 1.465ms
+// test 100000: 19.241ms, 5.709ms
 
 // How long does it take to double every number in a given 
 // array? 
@@ -60,3 +79,19 @@ let resultsInsert = perf.stop();
 console.log('Results for the extraLargeArray');
 console.log("insert", resultsInsert.preciseWords);
 console.log("append", resultsAppend.preciseWords);
+
+
+function find_longest_word(str) {
+    let words = str.split(' ');
+    let maxLength = 0;
+  
+    for (let i = 0; i < words.length; i++) {
+      if (words[i].length > maxLength) {
+        maxLength = words[i].length;
+      }
+    }
+    console.log(maxLength);
+  }
+//   I think the time complexity of this one would be O(n)
+  
+find_longest_word("I find coding to be hard, but fun");
